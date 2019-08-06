ce3c014981e5ecaec90b7c4c5f993c5a11e9d340
chetanpatel8476 <chetanpatel671@gmail.com>
2019-08-06 22:45:11 +0530

    Adding Student application

---
+++ src/main/java/com/einfochips/student/dto/StudentDTO.java
@@ -0,0 +1,60 @@
+package com.einfochips.student.dto;
+
+public class StudentDTO {
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Long phoneNumber;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("StudentDTO [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append(", email=").append(email).append(", phoneNumber=").append(phoneNumber)
+				.append("]");
+		return builder.toString();
+	}
+
+}
