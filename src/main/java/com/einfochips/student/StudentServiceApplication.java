6762c0cc18a6a340fe0f0c6300523044bd2d7c11
chetanpatel8476 <chetanpatel671@gmail.com>
2019-08-14 00:33:19 +0530

    Update pom.xml and mail application file

--- src/main/java/com/einfochips/student/StudentServiceApplication.java
+++ src/main/java/com/einfochips/student/StudentServiceApplication.java
@@ -2,9 +2,10 @@ package com.einfochips.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentServiceApplication {
+public class StudentServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
